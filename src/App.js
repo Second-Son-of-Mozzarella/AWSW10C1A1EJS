4b8bce1873c4e37754ab91d3bea15f01e7c4c9ea
Second-Son-of-Mozzarella <Mohtzee@protonmail.com>
2024-02-26 17:14:23 -0600

    1.01

---
+++ src/App.js
@@ -0,0 +1,162 @@
+// Repository:  medals-b-react
+// Author:      Jeff Grissom
+// Version:     4.xx
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import Country from './components/Country';
+import NewCountry from './components/NewCountry';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Badge from 'react-bootstrap/Badge';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './App.css';
+
+const App = () => {
+  const apiEndpoint = "https://medals-api-6.azurewebsites.net/api/country";
+  const [ countries, setCountries ] = useState([]);
+  const medals = useRef([
+    { id: 1, name: 'gold' },
+    { id: 2, name: 'silver' },
+    { id: 3, name: 'bronze' },
+  ]);
+
+  // this is the functional equivalent to componentDidMount
+  useEffect(() => {
+    // initial data loaded here
+    async function fetchCountries() {
+      const { data : fetchedCountries } = await axios.get(apiEndpoint);
+      // we need to save the original medal count values in state
+      let newCountries = [];
+      fetchedCountries.forEach(country => {
+        let newCountry = {
+          id: country.id,
+          name: country.name,
+        };
+        medals.current.forEach(medal => {
+          const count = country[medal.name];
+          // page_value is what is displayed on the web page
+          // saved_value is what is saved to the database
+          newCountry[medal.name] = { page_value: count, saved_value: count };
+        });
+        newCountries.push(newCountry);
+      });
+      setCountries(newCountries);
+    }
+    fetchCountries();
+  }, []);
+
+  const handleAdd = async (name) => {
+    const { data: post } = await axios.post(apiEndpoint, { name: name });
+    let newCountry = { 
+      id: post.id, 
+      name: post.name,
+    };
+    medals.current.forEach(medal => {
+      const count = post[medal.name];
+      // when a new country is added, we need to store page and saved values for
+      // medal counts in state
+      newCountry[medal.name] = { page_value: count, saved_value: count };
+    });
+    setCountries(countries.concat(newCountry));
+  }
+  const handleDelete = async (countryId) => {
+    const originalCountries = countries;
+    setCountries(countries.filter(c => c.id !== countryId));
+    try {
+      await axios.delete(`${apiEndpoint}/${countryId}`);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404) {
+        // country already deleted
+        console.log("The record does not exist - it may have already been deleted");
+      } else { 
+        alert('An error occurred while deleting');
+        setCountries(originalCountries);
+      }
+    }
+  }
+  const handleSave = async (countryId) => {
+    const originalCountries = countries;
+
+    const idx = countries.findIndex(c => c.id === countryId);
+    const mutableCountries = [ ...countries ];
+    const country = mutableCountries[idx];
+    let jsonPatch = [];
+    medals.current.forEach(medal => {
+      if (country[medal.name].page_value !== country[medal.name].saved_value) {
+        jsonPatch.push({ op: "replace", path: medal.name, value: country[medal.name].page_value });
+        country[medal.name].saved_value = country[medal.name].page_value;
+      }
+    });
+    console.log(`json patch for id: ${countryId}: ${JSON.stringify(jsonPatch)}`);
+    // update state
+    setCountries(mutableCountries);
+
+    try {
+      await axios.patch(`${apiEndpoint}/${countryId}`, jsonPatch);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404) {
+        // country already deleted
+        console.log("The record does not exist - it may have already been deleted");
+      } else { 
+        alert('An error occurred while updating');
+        setCountries(originalCountries);
+      }
+    }
+  }
+  const handleReset = (countryId) => {
+    // to reset, make page value the same as the saved value
+    const idx = countries.findIndex(c => c.id === countryId);
+    const mutableCountries = [ ...countries ];
+    const country = mutableCountries[idx];
+    medals.current.forEach(medal => {
+      country[medal.name].page_value = country[medal.name].saved_value;
+    });
+    setCountries(mutableCountries);
+  }
+  const handleIncrement = (countryId, medalName) => handleUpdate(countryId, medalName, 1);
+  const handleDecrement = (countryId, medalName) => handleUpdate(countryId, medalName, -1);
+  const handleUpdate = (countryId, medalName, factor) => {
+    const idx = countries.findIndex(c => c.id === countryId);
+    const mutableCountries = [...countries ];
+    mutableCountries[idx][medalName].page_value += (1 * factor);
+    setCountries(mutableCountries);
+  }
+  const getAllMedalsTotal = () => {
+    let sum = 0;
+    // use medal count displayed in the web page for medal count totals
+    medals.current.forEach(medal => { sum += countries.reduce((a, b) => a + b[medal.name].page_value, 0); });
+    return sum;
+  }
+  return (
+    <React.Fragment>
+    <Navbar className="navbar-dark bg-dark">
+        <Container fluid>
+          <Navbar.Brand>
+            Olympic Medals
+            <Badge className="ml-2" bg="light" text="dark" pill>{ getAllMedalsTotal() }</Badge>
+          </Navbar.Brand>
+          <NewCountry onAdd={ handleAdd } />
+        </Container>
+    </Navbar>
+    <Container fluid>
+    <Row>
+      { countries.map(country => 
+        <Col className="mt-3" key={ country.id }>
+          <Country  
+            country={ country } 
+            medals={ medals.current }
+            onDelete={ handleDelete }
+            onSave={ handleSave }
+            onReset={ handleReset }
+            onIncrement={ handleIncrement } 
+            onDecrement={ handleDecrement } />
+        </Col>
+      )}
+      </Row>
+    </Container>
+    </React.Fragment>
+  );
+}
+ 
+export default App;
---
+++ src/components/Country.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Medal from './Medal';
+import { TrashFill, FolderPlus, Trash3, } from 'react-bootstrap-icons';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const Country = (props) => {
+  const { country, medals, onIncrement, onDecrement, onDelete, onSave, onReset } = props;
+  const getMedalsTotal = (country, medals) => {
+    let sum = 0;
+    // use medal count displayed in the web page for medal count totals
+    medals.forEach(medal => { sum += country[medal.name].page_value; });
+    return sum;
+  }
+  const renderSaveButton = () => {
+    let unsaved = false;
+    medals.forEach(medal => {
+      if (country[medal.name].page_value !== country[medal.name].saved_value) {
+        unsaved = true;
+      }
+    });
+    return unsaved;
+  }
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title className="d-flex justify-content-between">
+          <span>
+          { country.name }
+          <Badge bg="secondary" text="light" pill className="ml-2">
+            { getMedalsTotal(country, medals) }
+          </Badge>
+          </span>
+          {/* this will render save/reset buttons if the page/saved medal counts are not equal
+          otherewise, the delete country button will be rendered */}
+          { renderSaveButton() ?
+            <React.Fragment>
+              {/* TODO: use Bootstrap stying / icons */}
+              <Trash3 onClick={ () => onReset(country.id) } className='icon-btn' />
+              <FolderPlus onClick={ () => onSave(country.id) } className='icon-btn' />
+              
+            </React.Fragment>
+            :
+            <TrashFill onClick={() => onDelete(country.id)} className='icon-btn' style={{ color:'red' }} />
+          }
+        </Card.Title>
+        <ListGroup variant="flush">
+        { medals.map(medal =>
+        <ListGroup.Item className="d-flex justify-content-between" key={ medal.id }>
+          <Medal  
+            country={ country } 
+            medal={ medal } 
+            onIncrement={ onIncrement } 
+            onDecrement={ onDecrement } />
+        </ListGroup.Item>
+        ) }
+        </ListGroup>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Country;
